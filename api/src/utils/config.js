0410364c54b166c33258e5270fb3061c58f7c0e7
fabiancord <fabianarielcordobes@gmail.com>
2023-10-10 20:04:02 -0300

    solucionando errores de deploy back

--- api/src/utils/config.js
+++ api/src/utils/config.js
@@ -8,6 +8,6 @@ module.exports = {
   DB_USER: env.DB_USER,
   DB_PASSWORD: env.DB_PASSWORD,
   DB_HOST: env.DB_HOST,
-  URL: env.URL,
+  URL: "https://pi-dogs-texg.onrender.com/",
   API_KEY: env.API_Key,
 };
